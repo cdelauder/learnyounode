bbef07767974408b89a2204996ded41d0cdb8478
cdelauder <cdelauder@gmail.com>
2014-08-08 10:31:41 -0700

    on step 4, it may finally start getting a little complicated.

--- stream.js
+++ stream.js
@@ -4,5 +4,14 @@
 
 //////////////////////////////step 2
 
-var fs = require('fs');
-fs.createReadStream(process.argv[2]).pipe(process.stdout);
+// var fs = require('fs');
+// fs.createReadStream(process.argv[2]).pipe(process.stdout);
+
+///////////////////////////////step 3
+
+// process.stdin.pipe(process.stdout)
+
+///////////////////////////////step 4
+
+var through = require('through')
+
