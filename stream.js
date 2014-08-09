e5a52c1735fa137345e43d9dd3d29176261f2aed
cdelauder <cdelauder@gmail.com>
2014-08-09 12:26:44 -0700

    only had a few minutes to hack on it today and couldnt get it working

--- stream.js
+++ stream.js
@@ -13,5 +13,11 @@
 
 ///////////////////////////////step 4
 
-var through = require('through')
+var through = require('through');
+var tr = through(write, end);
+tr.write('beep\n');
+tr.write('boop\n');
+tr.end();
 
+function write (buf) { console.dir(buf) }
+function end () { console.log('__END__') }
