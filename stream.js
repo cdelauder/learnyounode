7f3b2b84ce063f146e66e228f6da8f26683a8051
cdelauder <cdelauder@gmail.com>
2014-08-22 15:06:20 -0700

    figured out the duplexer. I was adding cmd and args when I merely needed to pass them as separate arguments. need to read more about spawn

--- stream.js
+++ stream.js
@@ -106,6 +106,7 @@ var spawn = require('child_process').spawn
 var duplex = require('duplexer')
 
 module.exports = function (cmd, args) {
-  duplex(process.stdin, process.stdout) 
+  var info = spawn(cmd, args)
+  return duplex(info.stdin, info.stdout) 
 }
 
