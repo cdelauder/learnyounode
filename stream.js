93beb63d680ba98b48dc7ffadeb1f6f7e6421551
cdelauder <cdelauder@gmail.com>
2014-09-01 10:21:37 -0700

    step13 finished. this one was a doozy. a lot of stuff going on. I am starting to wonder if this is the best way to go about node. I feel like it dives into packages instead of really illuminating the concepts required to get to know the framework

--- stream.js
+++ stream.js
@@ -130,8 +130,35 @@
 
 ////////////////////////////////////////////////step 13
 
-var combine = require('stream-combiner')
+var combine = require('stream-combiner');
+var through = require('through');
+var split = require('split');
+var zlib = require('zlib');
 
 module.exports = function () {
-  return combine()
-}
+    var grouper = through(write, end);
+    var current;
+    
+    function write (line) {
+        if (line.length === 0) return;
+        var row = JSON.parse(line);
+        
+        if (row.type === 'genre') {
+            if (current) {
+                this.queue(JSON.stringify(current) + '\n');
+            }
+            current = { name: row.name, books: [] };
+        }
+        else if (row.type === 'book') {
+            current.books.push(row.name);
+        }
+    }
+    function end () {
+        if (current) {
+            this.queue(JSON.stringify(current) + '\n');
+        }
+        this.queue(null);
+    }
+    
+    return combine(split(), grouper, zlib.createGzip());
+};
