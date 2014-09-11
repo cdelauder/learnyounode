efcfd7190c19f3e0ba37e14407d9409580b6cec1
cdelauder <cdelauder@gmail.com>
2014-09-11 15:34:21 -0700

    first attempt fail. I am confident I am missing something very simple here

--- stream.js
+++ stream.js
@@ -130,35 +130,45 @@
 
 ////////////////////////////////////////////////step 13
 
-var combine = require('stream-combiner');
-var through = require('through');
-var split = require('split');
-var zlib = require('zlib');
+// var combine = require('stream-combiner');
+// var through = require('through');
+// var split = require('split');
+// var zlib = require('zlib');
 
-module.exports = function () {
-    var grouper = through(write, end);
-    var current;
+// module.exports = function () {
+//     var grouper = through(write, end);
+//     var current;
     
-    function write (line) {
-        if (line.length === 0) return;
-        var row = JSON.parse(line);
+//     function write (line) {
+//         if (line.length === 0) return;
+//         var row = JSON.parse(line);
         
-        if (row.type === 'genre') {
-            if (current) {
-                this.queue(JSON.stringify(current) + '\n');
-            }
-            current = { name: row.name, books: [] };
-        }
-        else if (row.type === 'book') {
-            current.books.push(row.name);
-        }
-    }
-    function end () {
-        if (current) {
-            this.queue(JSON.stringify(current) + '\n');
-        }
-        this.queue(null);
-    }
+//         if (row.type === 'genre') {
+//             if (current) {
+//                 this.queue(JSON.stringify(current) + '\n');
+//             }
+//             current = { name: row.name, books: [] };
+//         }
+//         else if (row.type === 'book') {
+//             current.books.push(row.name);
+//         }
+//     }
+//     function end () {
+//         if (current) {
+//             this.queue(JSON.stringify(current) + '\n');
+//         }
+//         this.queue(null);
+//     }
     
-    return combine(split(), grouper, zlib.createGzip());
-};
+//     return combine(split(), grouper, zlib.createGzip());
+// };
+
+//////////////////////////////////////////////////step 14
+
+var crypto = require('crypto');
+var stream = crypto.createDecipher('aes256', process.argv[2]);  
+stream.pipe(process.stdin)
+stream.pipe(process.stdout);
+stream.end();
+
+
